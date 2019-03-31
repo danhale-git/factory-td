f84e10268d047c71cef42fa2973125170fd21738
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-03-31 17:51:07 +0100

    round height difference

--- Assets/Scripts/MapSystems/MeshDataSystem.cs
+++ Assets/Scripts/MapSystems/MeshDataSystem.cs
@@ -117,7 +117,7 @@ public class MeshDataSystem : ComponentSystem
                         
                         float distance = matrix.GetItem<WorleyNoise.PointData>(new int2(x, z), worley, arrayUtil).distance2Edge;
 
-                        if(difference > 1) color = new float4(0.7f, 0.7f, 0.7f, 1);
+                        if(math.round(difference) > 1) color = new float4(0.7f, 0.7f, 0.7f, 1);
                         else color = new float4(0.4f, 0.8f, 0f, 1);
                         //else color = new float4(distance, distance, distance, 1);
 
