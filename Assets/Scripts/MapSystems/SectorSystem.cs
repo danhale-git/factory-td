7a8e48e579de3a0da85d25197148b2c0a724fdc8
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-04-09 12:29:21 +0100

    refactoring

--- Assets/Scripts/MapSystems/SectorSystem.cs
+++ Assets/Scripts/MapSystems/SectorSystem.cs
@@ -9,6 +9,10 @@ public class SectorSystem : ComponentSystem
 {
     EntityManager entityManager;
 
+    Biomes biomes;
+    SimplexNoiseGenerator heightSimplex;
+    SimplexNoiseGenerator groupSimplex;
+
     public enum SectorTypes { NONE, UNPATHABLE }
     public struct SectorType : IComponentData { public SectorTypes Value; }
     
@@ -30,6 +34,10 @@ public class SectorSystem : ComponentSystem
     {
         entityManager = World.Active.GetOrCreateManager<EntityManager>();
 
+        biomes = new Biomes();
+        heightSimplex = TerrainSettings.HeightSimplex();
+        groupSimplex = TerrainSettings.GroupSimplex();
+
         EntityArchetypeQuery sectorQuery = new EntityArchetypeQuery{
             All = new ComponentType[] { typeof(Cell) },
             None = new ComponentType[] { typeof(SectorType) }
@@ -38,6 +46,11 @@ public class SectorSystem : ComponentSystem
     }
 
     protected override void OnUpdate()
+    {
+        CheckWorleyGeneration();
+    }
+
+    void CheckWorleyGeneration()
     {
         EntityCommandBuffer commandBuffer = new EntityCommandBuffer(Allocator.TempJob);
         NativeArray<ArchetypeChunk> chunks = sectorGroup.CreateArchetypeChunkArray(Allocator.TempJob);
@@ -55,21 +68,21 @@ public class SectorSystem : ComponentSystem
             {
                 DynamicBuffer<Cell> cellBuffer = cellBuffers[e];
 
-                float value = 0;
-                for(int i = 0; i < cellBuffer.Length; i++)
+                if(!AllEntitiesHaveWorley(cellBuffer))
                 {
-                    value += cellBuffer[i].data.value;
-                }
-                value /= cellBuffer.Length;
-
-                Debug.Log(value);
-
-                for(int i = 0; i < cellBuffer.Length; i++)
-                {
-                    commandBuffer.AddComponent<SectorValue>(cellBuffer[i].entity, new SectorValue{ Value = value });
+                    continue;
                 }
 
-                commandBuffer.AddComponent(entities[e], new SectorType());
+                float value = GetSectorValue(cellBuffer);
+
+                SectorType type = new SectorType{ Value = SectorTypes.NONE };
+
+                if(!SectorIsPathable(cellBuffer))
+                    type.Value = SectorTypes.UNPATHABLE;
+
+                AddSectorComponents(value, type, cellBuffer, commandBuffer);                
+
+                commandBuffer.AddComponent(entities[e], type);
             }
         }
 
@@ -77,5 +90,61 @@ public class SectorSystem : ComponentSystem
         commandBuffer.Dispose();
 
         chunks.Dispose();
+    }
+
+    bool AllEntitiesHaveWorley(DynamicBuffer<Cell> cellBuffer)
+    {
+        for(int i = 0; i < cellBuffer.Length; i++)
+        {
+            if( !entityManager.HasComponent(cellBuffer[i].entity, typeof(WorleyNoise.PointData)) )
+                return false;
+        }
+        return true;
+    }
+
+    float GetSectorValue(DynamicBuffer<Cell> cellBuffer)
+    {
+        float value = 0;
+        for(int i = 0; i < cellBuffer.Length; i++)
+            value += cellBuffer[i].data.value;
+
+        return value / cellBuffer.Length;
+    }
+
+    void AddSectorComponents(float value, SectorType type, DynamicBuffer<Cell> cellBuffer, EntityCommandBuffer commandBuffer)
+    {
+        for(int i = 0; i < cellBuffer.Length; i++)
+        {
+            commandBuffer.AddComponent<SectorValue>(cellBuffer[i].entity, new SectorValue{ Value = value });
+            commandBuffer.AddComponent<SectorType>(cellBuffer[i].entity, type);
+        }
+    }
+
+    bool SectorIsPathable(DynamicBuffer<Cell> cellBuffer)
+    {
+        for(int i = 0; i < cellBuffer.Length; i++)
+        {
+            DynamicBuffer<WorleyNoise.PointData> points = entityManager.GetBuffer<WorleyNoise.PointData>(cellBuffer[i].entity);
+            for(int p = 0; p < points.Length; p++)
+            {
+                WorleyNoise.PointData point = points[p];
+
+                float currentCellGroup = biomes.CellGrouping(point.currentCellIndex, groupSimplex, heightSimplex);
+                float adjacentCellGroup = biomes.CellGrouping(point.adjacentCellIndex, groupSimplex, heightSimplex);
+
+                if(currentCellGroup == adjacentCellGroup) continue;
+
+                float currentCellHeight = biomes.CellHeight(point.currentCellIndex, heightSimplex);
+                float adjacentCellHeight = biomes.CellHeight(point.adjacentCellIndex, heightSimplex);
+
+                if(currentCellHeight == adjacentCellHeight) return true;
+
+                int2 adjacentDirection = point.adjacentCellIndex - point.currentCellIndex;
+
+                if(biomes.EdgeIsSloped(adjacentDirection, point.currentCellValue, point.adjacentCellValue))
+                    return true;
+            }
+        }
+        return false;
     }
 }
