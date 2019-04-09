a7ef1cf83095385a66e636f91a992bb17a030d3b
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-04-09 21:14:19 +0100

    Worley tests

--- Assets/Tests/WorleyNoiseTests.cs
+++ Assets/Tests/WorleyNoiseTests.cs
@@ -13,48 +13,100 @@ namespace Tests
         WorleyNoise cellWorley = TerrainSettings.CellWorley();
 
         [Test]
-        public void GetPointData_returns_values_greater_than_zero()
+        public void Cell_value_greater_than_zero_and_less_than_one()
         {
-            WorleyNoise.PointData point = RandomPointData();
+            for(int i = 0; i < 500; i++)
+            {
+                WorleyNoise.CellData cell = RandomCellData();
+                WorleyNoise.PointData point = RandomPointData();
 
-            float sumOfAllValues = 0;
-
-            sumOfAllValues += point.distance2Edge;
-            sumOfAllValues += point.distance;
-
-            sumOfAllValues += point.currentCellValue;
-            sumOfAllValues += point.adjacentCellValue;
-
-            sumOfAllValues += point.currentCellPosition.x;
-            sumOfAllValues += point.currentCellPosition.z;
-            sumOfAllValues += point.adjacentCellPosition.x;
-            sumOfAllValues += point.adjacentCellPosition.z;
-
-            sumOfAllValues += point.currentCellIndex.x;
-            sumOfAllValues += point.currentCellIndex.y;
-            sumOfAllValues += point.adjacentCellIndex.x;
-            sumOfAllValues += point.adjacentCellIndex.y;
-
-            Assert.NotZero(sumOfAllValues);
+                Assert.Less(cell.value, 1, "Cell less than 1");
+                Assert.Greater(cell.value, 0, "Cell greater than 0");
+                Assert.Less(point.currentCellValue, 1, "Point less than 1");
+                Assert.Greater(point.currentCellValue, 0, "Point greater than 0");
+            }
         }
 
         [Test]
-        public void Adjacent_position_matches_adjacent_cell_current_position()
+        public void Returns_values_greater_than_zero()
+        {
+            WorleyNoise.CellData cell = RandomCellData();
+            WorleyNoise.PointData point = RandomPointData();
+
+            float sumOfAllValuesPoint = 0;
+            float sumOfAllValuesCell = 0;
+
+            sumOfAllValuesCell += cell.index.x;
+            sumOfAllValuesCell += cell.index.y;
+            sumOfAllValuesCell += cell.position.x;
+            sumOfAllValuesCell += cell.position.z;
+            sumOfAllValuesCell += cell.value;
+
+            sumOfAllValuesPoint += point.distance2Edge;
+            sumOfAllValuesPoint += point.distance;
+
+            sumOfAllValuesPoint += point.currentCellValue;
+            sumOfAllValuesPoint += point.adjacentCellValue;
+
+            sumOfAllValuesPoint += point.currentCellPosition.x;
+            sumOfAllValuesPoint += point.currentCellPosition.z;
+            sumOfAllValuesPoint += point.adjacentCellPosition.x;
+            sumOfAllValuesPoint += point.adjacentCellPosition.z;
+
+            sumOfAllValuesPoint += point.currentCellIndex.x;
+            sumOfAllValuesPoint += point.currentCellIndex.y;
+            sumOfAllValuesPoint += point.adjacentCellIndex.x;
+            sumOfAllValuesPoint += point.adjacentCellIndex.y;
+
+            Assert.NotZero(sumOfAllValuesPoint, "Point");
+            Assert.NotZero(sumOfAllValuesCell, "Cell");
+        }
+
+        [Test]
+        public void Distances_do_not_equal_999999()
+        {
+            WorleyNoise.PointData point = RandomPointData();
+
+            bool notEqual = (   point.distance2Edge != 999999   &&
+                                point.distance      != 999999   );
+
+            Assert.IsTrue(notEqual, "distance2Edge"+point.distance2Edge+"\ndistance"+point.distance);
+        }
+
+        [Test]
+        public void PointData_matches_CellData()
         {
             WorleyNoise.PointData randomPoint = RandomPointData();
             WorleyNoise.CellData cell = cellWorley.GetCellData(randomPoint.currentCellIndex);
 
-            Assert.IsTrue(randomPoint.currentCellPosition.Equals(cell.position));
+            Assert.IsTrue(
+                randomPoint.currentCellPosition.Equals(cell.position),
+                "Position\n"+"PointData: "+randomPoint.currentCellPosition+'\n'+"CellData: "+cell.position
+            );
+            Assert.IsTrue(
+                randomPoint.currentCellIndex.Equals(cell.index),
+                "Index\n"+"PointData: "+randomPoint.currentCellIndex+'\n'+"CellData: "+cell.index
+            );
+            Assert.IsTrue(
+                randomPoint.currentCellValue.Equals(cell.value),
+                "Value\n"+"PointData: "+randomPoint.currentCellValue+'\n'+"CellData: "+cell.value
+            );
         }
 
         WorleyNoise.PointData RandomPointData()
         {
-            Unity.Mathematics.Random random = new Unity.Mathematics.Random(123456);
-
-            int x = random.NextInt(0, 500);
-            int z = random.NextInt(0, 500);
+            int x = UnityEngine.Random.Range(-5000, 5000);
+            int z = UnityEngine.Random.Range(-5000, 5000);
 
             return cellWorley.GetPointData(x, z);
+        }
+
+        WorleyNoise.CellData RandomCellData()
+        {
+            int x = UnityEngine.Random.Range(-500, 500);
+            int z = UnityEngine.Random.Range(-500, 500);
+
+            return cellWorley.GetCellData(new int2(x, z));
         }
         
     }
