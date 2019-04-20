ea63656f9c1811c507c4947ca0a5e011f5c16287
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-04-20 11:22:09 +0100

    CellSystem tests

---
+++ Assets/Tests_playmode/CellSystemTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Unity.Entities;
+
+namespace Tests
+{
+    public class CellSystemTests
+    {
+        [Test]
+        public void Initial_cell_is_created()
+        {
+            CellSystem cellSystem = World.Active.GetOrCreateSystem<CellSystem>();
+            
+            Entity currentCellEntity;
+            bool currentCellExists = cellSystem.TryGetCell(cellSystem.currentCellIndex, out currentCellEntity);
+            Assert.IsTrue(currentCellExists);
+        }
+
+        [Test]
+        public void Initial_cell_has_worley_data()
+        {
+            Assert.IsTrue(CellHasData(CurrentCellEntity()));
+        }
+
+        [Test]
+        public void Adjacent_cells_are_created()
+        {
+            CellSystem cellSystem = World.Active.GetOrCreateSystem<CellSystem>();
+
+            DynamicBuffer<CellSystem.AdjacentCell> adjacentCells = World.Active.EntityManager.GetBuffer<CellSystem.AdjacentCell>(CurrentCellEntity());
+
+            bool noCellsMissing = false;
+
+            for(int i = 0; i < adjacentCells.Length; i++)
+            {
+                Entity adjacentCellEntity;
+                if(!cellSystem.TryGetCell(adjacentCells[i].data.index, out adjacentCellEntity))
+                {
+                    noCellsMissing = false;
+                    break;
+                }
+                else
+                    noCellsMissing = true;
+            }
+
+            Assert.IsTrue(noCellsMissing);
+        }
+
+        Entity CurrentCellEntity()
+        {
+            CellSystem cellSystem = World.Active.GetOrCreateSystem<CellSystem>();
+
+            Entity currentCellEntity;
+            cellSystem.TryGetCell(cellSystem.currentCellIndex, out currentCellEntity);
+
+            return currentCellEntity;
+        }
+
+        bool CellHasData(Entity cellEntity)
+        {
+            EntityManager entityManager = World.Active.EntityManager;
+            if(!entityManager.Exists(cellEntity))
+                return false;
+
+            return entityManager.HasComponent<WorleyNoise.PointData>(cellEntity);
+        }
+    }
+}
--- Assets/Tests_playmode/WorldTests.cs
+++ Assets/Tests_playmode/WorldTests.cs
@@ -15,39 +15,5 @@ namespace Tests
             World defaultWorld = World.Active;
             Assert.NotNull(defaultWorld);
         }
-
-        [Test]
-        public void Initial_cell_is_created()
-        {
-            CellSystem cellSystem = World.Active.GetOrCreateSystem<CellSystem>();
-            
-            Entity currentCellEntity;
-            bool currentCellExists = cellSystem.TryGetCell(cellSystem.currentCellIndex, out currentCellEntity);
-            Assert.IsTrue(currentCellExists);
-        }
-
-        [Test]
-        public void Initial_cell_has_worley_data()
-        {
-            EntityManager entityManager = World.Active.EntityManager;
-
-            CellSystem cellSystem = World.Active.GetOrCreateSystem<CellSystem>();
-            
-            Entity currentCellEntity;
-            cellSystem.TryGetCell(cellSystem.currentCellIndex, out currentCellEntity);
-
-            DynamicBuffer<WorleyNoise.PointData> worleyData = entityManager.GetBuffer<WorleyNoise.PointData>(currentCellEntity);
-
-            bool pointIsPopulated = false;
-
-            for(int i = 0; i < worleyData.Length; i++)
-            {
-                pointIsPopulated = worleyData[i].isSet;
-                if(pointIsPopulated)
-                    break;
-            }
-
-            Assert.IsTrue(pointIsPopulated);
-        }
     }
 }
